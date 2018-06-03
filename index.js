d1edb6035ac9d628186570e03a27bf8889952061
Shepless <chris.shepherd1985@gmail.com>
2018-06-03 20:56:59 +0100

    Added basic documentation

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const {discoverDevices} = require('./lib/discovery');
 
 module.exports = {
+  /** Find all brains connected to the local network
+  * @return {NeeoBrain[]} All discovered brains on the local network
+  */
   discoverBrains: discoverDevices
 };
